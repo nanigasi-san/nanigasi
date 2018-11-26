64cdf166ebbaf56fa246434e14eee684b88be5a5
nanigasi <nanigasi.py@gmail.com>
2018-11-26 23:13:20 +0900

    これで行けるんとちゃう？？？？？

--- Atcoder/Shift_only.py
+++ Atcoder/Shift_only.py
@@ -1,11 +1,21 @@
+import sys
 N = int(input())
-number = [int(i) for i in input().split()]
-print(number)
-re = 0
-for num in number:
-    if num%2 == 0:
-        num = num/2
-        re += 1
-    else:
-        print(re)
-        break
+list = list(map(int,input().split()))
+num = 0
+while True:
+    fnum = 0
+    for i in list:
+        a = i/2
+        a_ = int(a)
+        if a != a_ :
+            print(num)
+            sys.exit()
+        elif a == 0:
+            print(num)
+            sys.exit()
+        else:
+            list[fnum] = a
+            fnum += 1
+    num += 1
+
+print(num)
