3f038319e405ef8cebc501b78816b963572a2c66
nanigasi <nanigasi.py@gmail.com>
2018-11-28 04:11:47 +0900

    多分いけたやろ

---
+++ Atcoder/ABC/100-C.py
@@ -0,0 +1,14 @@
+#yet
+N = int(input())
+A = [i for i in map(int,input().split())]
+count = 0
+for a in A:
+    while True:
+        harf = a/2
+        if harf == int(harf):
+            a = harf
+            count += 1
+            continue
+        else:
+            break
+print(count)
