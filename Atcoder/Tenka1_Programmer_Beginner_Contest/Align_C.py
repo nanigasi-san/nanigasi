515986c12ecd8945bc75f83bea8ccdcdfa914c55
nanigasi <nanigasi.py@gmail.com>
2018-11-28 20:31:35 +0900

    C_1

---
+++ Atcoder/Tenka1_Programmer_Beginner_Contest/Align_C.py
@@ -0,0 +1,16 @@
+#yet
+N = int(input())
+A = [int(input()) for _ in range(N)]
+A.sort(reverse=True)
+A_ = ["X"]
+print(A)
+a = 0
+b = 0
+if N%2 != 0:
+    while b+2 <= N:
+        A_.append(A[a])
+        a += 1
+        A_.append(A[N-a])
+        b += 2
+    A_[0] = A[a]
+print(A_)
