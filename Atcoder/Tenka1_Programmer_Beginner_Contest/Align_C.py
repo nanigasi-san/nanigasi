feac9cb2f72dbf2dede208f4ac02ef850e99967c
nanigasi <nanigasi.py@gmail.com>
2018-11-29 18:40:30 +0900

    全くダメで草

--- Atcoder/Tenka1_Programmer_Beginner_Contest/Align_C.py
+++ Atcoder/Tenka1_Programmer_Beginner_Contest/Align_C.py
@@ -1,20 +1,28 @@
 #yet
 N = int(input())
+M = N-1
+n = N//2
+m = M//2
 A = [int(input()) for _ in range(N)]
 A.sort(reverse=True)
-A_ = ["X"]
-print(A)
-a = 0
-b = 0
-if N % 2 != 0:
-    while b+2 <= N:
-        A_.append(A[a])
-        a += 1
-        A_.append(A[N-a])
-        b += 2
-    A_[0] = A[a]
-print(A_)
-ABS = 0
-for _ in range(N):
-    ABS += abs(A_[_]-A_[_+1])
-print(ABS)
+A_ = []
+num = 0
+
+if N >= 4:
+    A_.append(A[n])
+    for _ in range(m):
+        A_.append(A[_])
+        A_.append(A[M-_])
+    if n != m:
+        A_.append(A[m])#A_完了
+    for _ in range(M):
+        num += (abs((A_[_])-(A_[_+1])))
+    print(num)
+elif N == 3:
+    A[1],A[2] = A[2],A[1]
+    abs1 = abs(A[0]-A[1])
+    abs2 = abs(A[1]-A[2])
+    print(abs1+abs2)
+
+else:
+    print(A[0]-A[1])
