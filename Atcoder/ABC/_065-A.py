c7d5d41fdfd594684e688381cfd5722e9ff06085
nanigasi <nanigasi.py@gmail.com>
2018-11-30 01:33:35 +0900

    #go

---
+++ Atcoder/ABC/_065-A.py
@@ -0,0 +1,11 @@
+X,A,B = map(int,input().split())
+
+if A >= B:#umai
+    print("delicious")
+elif A < B:#mazui
+    over = B-A
+    if over <= X:
+        print("safe")
+    else:
+        print("dangerous")
+#go
