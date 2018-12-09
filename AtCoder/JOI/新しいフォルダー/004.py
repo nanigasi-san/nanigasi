2bb11c0d742a4c386067a87f280fae70e85f3597
nanigasi <nanigasi.py@gmail.com>
2018-12-09 18:25:55 +0900

    imiwakarann:

--- AtCoder/JOI/新しいフォルダー/004.py
+++ AtCoder/JOI/新しいフォルダー/004.py
@@ -1,9 +1,12 @@
 N = int(input())
 A = list(map(int,input().split()))
 High = [n for n in range(max(A))]
-gnum = 0
-
+High2 = []
 for high in High:
+    if A.count(high) != 0:
+        High2.append(high)
+gnum = 0
+for high in High2:
     num = 0
     sw = 0
     for a in A:
