aa85bc32fa8b2cbd20b8076350b5378b54091c0a
nanigasi <nanigasi.py@gmail.com>
2018-12-12 19:30:54 +0900

    AC

--- AtCoder/ABC/_003-B.py
+++ AtCoder/ABC/_003-B.py
@@ -3,12 +3,13 @@ c1 = list(map(str,input().split()))
 c2 = list(map(str,input().split()))
 c3 = list(map(str,input().split()))
 
-list = [c0+c1+c2+c3]
-print(list)
+list = c0+c1+c2+c3
 list2 = []
-for i in range(len(list),0):
+for i in range(len(list),0,-1):
     list2.append(list[i-1])
 counter = 0
-for x in range(4):
-    print(list2[x+counter],list2[x+1+counter],list2[x+2+counter],list2[x+3+counter])
-    counter += 4
+for i in range(4):
+    xxx = i+counter
+    print(list2[xxx],list2[xxx+1],list2[xxx+2],list2[xxx+3])
+    counter += 3
+#ac
---
+++ AtCoder/ABC/_005-B.py
@@ -0,0 +1,5 @@
+N = int(input())
+list = [int(input()) for _ in range(N)]
+list.sort()
+print(list[0])
+#ac
