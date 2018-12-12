0400d846fb1528f8e34ed6ef302f03955bb932bb
nanigasi <nanigasi.py@gmail.com>
2018-12-12 10:25:53 +0900

    #go

--- AtCoder/ABC/_002-B.py
+++ AtCoder/ABC/_002-B.py
@@ -1,6 +1,13 @@
 W =  list(input())
 No = ["a","i","u","e","o"]
 list = []
-
+for w in W:
+    counter = 0
+    for no in No:
+        if w == no:
+            break
+        counter += 1
+        if counter == 5:
+            list.append(w)
 print("".join(list))
-#no
+#go
