39ae7bf4e16a88f71cb47156aea1ea4726d4bd01
nanigasi <nanigasi.py@gmail.com>
2018-12-14 01:17:52 +0900

    add exit()
    "Save"による.txtの保存 "Exit"によるウィンドウの消去
    
    を備えたテキストエディタを製作(していない)

---
+++ library/GUI/Tkinter/nanigasi_editer.py
@@ -0,0 +1,30 @@
+from tkinter import Tk,Button,filedialog
+from tkinter.scrolledtext import ScrolledText
+import tkinter
+import codecs
+
+root = Tk()
+root.geometry("500x400")
+root.title("editer")
+
+scrolledtext = ScrolledText(root,state='normal',font=("",12))
+scrolledtext.place(x=20,y=20,width=480,height=340)
+
+def save(self):
+    name = filedialog.asksaveasfilename()
+    text = scrolledtext.get("1.0",tkinter.END)
+
+    f = codecs.open(name,"w","utf-8")
+    f.write(text)
+    f.close()
+
+btn = Button(root,text="Save")
+btn.place(x=20,y=370,width=80,height=20)
+btn.bind("<ButtonRelease-1>",save)
+
+btnex = Button(root)
+btnex.config(text="Exit")
+btnex.place(x=110,y=370,width=80,height=20)
+btnex.bind("<Button-1>",exit)
+
+root.mainloop()
