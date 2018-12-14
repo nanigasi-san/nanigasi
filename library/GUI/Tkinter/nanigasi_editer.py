f1c1536b28947f5981bfffb9123cc4df7d6707ef
nanigasi <nanigasi.py@gmail.com>
2018-12-15 01:27:48 +0900

    -> conpact.

--- library/GUI/Tkinter/nanigasi_editer.py
+++ library/GUI/Tkinter/nanigasi_editer.py
@@ -4,11 +4,11 @@ import tkinter
 import codecs
 
 root = Tk()
-root.geometry("500x400+400+200")
+root.geometry("400x210+400+200")
 root.title("editer")
 
 scrolledtext = ScrolledText(root,state='normal',font=("",12))
-scrolledtext.place(x=20,y=20,width=480,height=340)
+scrolledtext.place(x=20,y=20,width=380,height=140)
 
 def save(self):
     name = filedialog.asksaveasfilename()
@@ -19,12 +19,12 @@ def save(self):
     f.close()
 
 btn = Button(root,text="Save")
-btn.place(x=20,y=370,width=80,height=20)
+btn.place(x=100,y=170,width=95,height=30)
 btn.bind("<ButtonRelease-1>",save)
 
 btnex = Button(root)
 btnex.config(text="Exit")
-btnex.place(x=110,y=370,width=80,height=20)
+btnex.place(x=200,y=170,width=95,height=30)
 btnex.bind("<Button-1>",exit)
 
 root.mainloop()
