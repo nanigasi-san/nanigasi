21b5f90a8e41a9e66c8e8161d4493b4a246414d3
nanigasi <nanigasi.py@gmail.com>
2018-12-25 11:55:28 +0900

    Use Excel by python.

---
+++ library/use_file/openpyxl/Writexlsx_4.py
@@ -0,0 +1,19 @@
+#フォント設定
+import openpyxl
+
+book = openpyxl.Workbook()
+
+active_sheet = book.active
+active_sheet.cell(column=1,row=1,value="Write A1")
+active_sheet["A2"] = "Write A2"
+
+cell_A1 = active_sheet["A1"]
+cell_A1.font = openpyxl.styles.Font(bold=True)
+
+font_A2 = openpyxl.styles.Font()
+font_A2.bold = True
+font_A2.color = openpyxl.styles.colors.BLUE
+font_A2.name = "Arial"
+active_sheet["A2"].font= font_A2
+
+book.save("library/use_file/openpyxl/sample4.xlsx")
