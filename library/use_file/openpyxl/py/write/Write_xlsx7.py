f15070a75bca448c084ad80526a3b2018cecdf93
nanigasi <nanigasi.py@gmail.com>
2018-12-29 00:10:00 +0900

    6,7,8,9 ->6,(7),(8)->7,(9)->8

--- library/use_file/openpyxl/py/write/Write_xlsx7.py
+++ library/use_file/openpyxl/py/write/Write_xlsx7.py
@@ -1,38 +1,35 @@
-# 塗りつぶし設定
+#文字寄せ設定
 import openpyxl
-from openpyxl.styles.colors import (
-    Color,
-    BLUE,
-    RED,
-    GREEN,
-)
-from openpyxl.styles.fills import (
-    PatternFill,
-    FILL_SOLID,
-    FILL_PATTERN_MEDIUMGRAY,
-    FILL_PATTERN_GRAY125,
-)
+from openpyxl.styles import Alignment
 
 book = openpyxl.Workbook()
 
 active_sheet = book.active
-active_sheet.cell(column=2, row=2, value="Write B2")
-active_sheet["C3"] = "Write C3"
-active_sheet["D4"] = "Write D4"
+active_sheet.cell(column=2,row=2,value="write B2")
+active_sheet["C3"] = "write C3"
+active_sheet["D4"] = "write D4"
+active_sheet["E5"] = "write E5"
 
-active_sheet["B2"].fill = PatternFill(
-    patternType=FILL_SOLID,
-    fgColor=openpyxl.styles.colors.Color(rgb=BLUE),
+active_sheet["B2"].alignment = Alignment(
+    horizontal="center",
+    vertical="bottom",
 )
 
-fill_C3 = PatternFill()
-fill_C3.patternType = FILL_PATTERN_MEDIUMGRAY
-fill_C3.fgColor = RED
-active_sheet["C3"].fill = fill_C3
+align_C3 = Alignment()
+align_C3.horizontal = "right"
+align_C3.vertical = "center"
+active_sheet["C3"].alignment = align_C3
 
-fill_D4 = PatternFill()
-fill_D4.patternType = FILL_PATTERN_GRAY125
-fill_D4.fgColor = GREEN
-active_sheet["D4"].fill = fill_D4
+
+align_D4 = Alignment()
+align_D4.horizontal = "left"
+align_D4.vertical = "center"
+active_sheet["D4"].alignment = align_D4
+
+
+align_E5 = Alignment()
+align_E5.horizontal = "left"
+align_E5.vertical = "top"
+active_sheet["E5"].alignment = align_E5
 
 book.save("library/use_file/openpyxl/xlsx/sample7.xlsx")
