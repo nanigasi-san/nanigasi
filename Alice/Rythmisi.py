57fb13b008ae41192f0b68429a8ff2698c184961
nanigasi <nanigasi.py@gmail.com>
2019-01-10 15:52:42 +0900

    define sympliroma

--- Alice/Rythmisi.py
+++ Alice/Rythmisi.py
@@ -1,17 +1,27 @@
 #被り防止でいきってギリシャ語つかおう
 #ギリシャ語で「集合」らしい
+'''
+数字のみしか使えない
+'''
 import sympy as sp
 x = sp.Symbol("x")
-class Dimiourgia():
+class Dimiourgia():#生成
 
-    def olokliro(self,list_obj=[]):#集合の生成
+    def olokliro(self,list_obj=[]):#全体集合の生成
         if isinstance(list_obj,list) == True:
             return sp.FiniteSet(*list_obj)
 
     def meros(self,oya,joukenn):#部分集合の生成
-        emp = []
+        emp1 = []
         for i in oya:
             ans = joukenn.subs(x,i)
             if ans == True:
-                emp.append(i)
-        return sp.FiniteSet(*emp)
+                emp1.append(i)
+        return sp.FiniteSet(*emp1)
+
+    def sympliroma(self,zentai,bubun):#補集合の生成
+        emp2 = []
+        for element in zentai:
+            if (element in bubun) == False:
+                emp2.append(element)
+        return sp.FiniteSet(*emp2)
--- moduletest.py
+++ moduletest.py
@@ -1,10 +1,13 @@
 import Alice.Rythmisi as al
 ry = al.Dimiourgia()
 x = al.x
-list=[23,52,672,23.0,235,23,"dsf"]
+list=[23,23.0,52,672,235,23]
 
-test = ry.olokliro(list)
-print(test)
+test1 = ry.olokliro(list)
+print(test1)
 
-test2 = ry.meros(test,x<100)
+test2 = ry.meros(test1,x<100)
 print(test2)
+
+test3 = ry.sympliroma(test1,test2)
+print(test3)
