4d2f19296998ad78845d6597436d03010d75d542
nanigasi <nanigasi.py@gmail.com>
2019-01-14 00:22:59 +0900

    sabunn

--- moduletest2.py
+++ moduletest2.py
@@ -1,3 +1,16 @@
 from Alice import Rythmisi
+import sympy as sp
+dm = Rythmisi.Dimiourgia()
 
-kr = Rythmisi.Krisi()
+list1 = [1,2,3,4,5,6,7,8,9,10]
+list2 = [2,4,6,8,10,12,14,16,18,20]
+
+test1 = dm.olokliro(list1)
+test2 = dm.olokliro(list2)
+
+print(test1,test2)
+
+x = dm.x
+expr = (x==3)
+test3 = dm.meros(test1,sp.sympify(expr))
+print(test3)
