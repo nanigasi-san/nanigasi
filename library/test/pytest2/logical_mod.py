78dbc70b25b265020d72b106b6d033ea16491219
nanigasi <nanigasi.py@gmail.com>
2019-03-03 06:43:03 +0900

    tyottonamaehennkou

--- library/test/pytest2/logical_mod.py
+++ library/test/pytest2/logical_mod.py
@@ -1,5 +1,5 @@
 import numpy as np
-def and_logical(a,b):
+def and_logical_easy(a,b):
     w1,w2,t = 0.5,0.5,0.7
     ans = a*w1+b*w2-t
     if ans > 0:
@@ -7,7 +7,7 @@ def and_logical(a,b):
     else:
         return 0
 
-def and_logical_array(x1,x2):
+def and_logical(x1,x2):
     x = np.array([x1,x2])
     w = np.array([0.5,0.5])
     b = -0.7
@@ -18,7 +18,7 @@ def and_logical_array(x1,x2):
     else:
         return 0
 
-def nand_logical_array(x1,x2):
+def nand_logical(x1,x2):
     x = np.array([x1,x2])
     w = np.array([-0.5,-0.5])
     b = 0.7
@@ -28,7 +28,7 @@ def nand_logical_array(x1,x2):
     else:
         return 0
 
-def or_logical_array(x1,x2):
+def or_logical(x1,x2):
     x = np.array([x1,x2])
     w = np.array([0.5,0.5])
     b = -0.3
--- library/test/pytest2/test_logical.py
+++ library/test/pytest2/test_logical.py
@@ -1,6 +1,17 @@
-from logical_mod import and_logical,and_logical_array,nand_logical_array,or_logical_array
+from logical_mod import and_logical_easy,and_logical,nand_logical,or_logical
 
 class TestClass1():
+    def test_and_1(self):
+        assert and_logical_easy(0,0)==0
+    def test_and_2(self):
+        assert and_logical_easy(0,1)==0
+    def test_and_3(self):
+        assert and_logical_easy(1,0)==0
+    def test_and_4(self):
+        assert and_logical_easy(1,1)==1
+
+#
+class TestClass2():
     def test_and_1(self):
         assert and_logical(0,0)==0
     def test_and_2(self):
@@ -10,34 +21,23 @@ class TestClass1():
     def test_and_4(self):
         assert and_logical(1,1)==1
 
-#
-class TestClass2():
-    def test_and_1(self):
-        assert and_logical_array(0,0)==0
-    def test_and_2(self):
-        assert and_logical_array(0,1)==0
-    def test_and_3(self):
-        assert and_logical_array(1,0)==0
-    def test_and_4(self):
-        assert and_logical_array(1,1)==1
-
 class TestClass3():
     def test_and_1(self):
-        assert nand_logical_array(0,0)==1
+        assert nand_logical(0,0)==1
     def test_and_2(self):
-        assert nand_logical_array(0,1)==1
+        assert nand_logical(0,1)==1
     def test_and_3(self):
-        assert nand_logical_array(1,0)==1
+        assert nand_logical(1,0)==1
     def test_and_4(self):
-        assert nand_logical_array(1,1)==0
+        assert nand_logical(1,1)==0
 
 #
 class TestClass4():
     def test_and_1(self):
-        assert or_logical_array(0,0)==0
+        assert or_logical(0,0)==0
     def test_and_2(self):
-        assert or_logical_array(0,1)==1
+        assert or_logical(0,1)==1
     def test_and_3(self):
-        assert or_logical_array(1,0)==1
+        assert or_logical(1,0)==1
     def test_and_4(self):
-        assert or_logical_array(1,1)==1
+        assert or_logical(1,1)==1
