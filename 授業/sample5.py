e7ee7be248134527e7935279fcc61f3e20c03da4
nanigasi <nanigasi.py@gmail.com>
2019-05-09 22:18:20 +0900

    授業おっぱいそん

---
+++ 授業/sample5.py
@@ -0,0 +1,16 @@
+from copy import copy,deepcopy
+a = ["nanigasi","kantarow","tky_sleep"]
+a
+b = a
+b
+b[1] = "sex"
+b
+a
+
+c = copy(a)
+c
+
+c[1] = "yuto"
+c
+a
+
---
+++ 授業/sample6.py
@@ -0,0 +1,8 @@
+"""x = 10
+y = 20
+print("x:{0} y:{1}".format(x,y))
+x,y = y,x
+print("x:{0} y:{1}".format(x,y))
+"""
+
+print('"Hello,world"')
