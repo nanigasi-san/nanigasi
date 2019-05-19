93a6430107f8bdfb2f838e4744fef80ff622a0aa
nanigasi <nanigasi.py@gmail.com>
2019-05-20 05:31:32 +0900

    2変数の漸化式に対応

--- library_make/nanigasi_sequence.py
+++ library_make/nanigasi_sequence.py
@@ -43,8 +43,47 @@ class Zennka1(Sequence):
     def __init__(self,first_term,recurrence_fomula):
         self.first_term = first_term
         self.recurrence_fomula = sympify(recurrence_fomula)
-    def zenka(self,end):
-        zenka_list = [self.first_term]
-        for _ in range(2,end+1):
-            zenka_list.append(self.recurrence_fomula.subs({"a_n":zenka_list[_-2],"n":_-1}))
-        return zenka_list
+
+    def enumeration(self,start,end):
+        num_log = None #前世の記憶
+        _ = 1
+        while _ < start:
+            if _ == 1:
+                num_log = self.first_term
+            else:
+                num_log = self.recurrence_fomula.subs({"a_n":num_log,"n":_})
+            _ += 1
+        for _ in range(start,end+1):
+            if _ == 1:
+                num_log = self.first_term
+            else:
+                num_log = self.recurrence_fomula.subs({"a_n":num_log,"n":_})
+            yield num_log
+
+
+class Zennka2(Sequence):
+    a_n = Symbol("a_n")
+    a_n_plus1 = Symbol("a_n_plus1")
+    second_term = None
+    recurrence_fomula = None # `a_n+2 = `の式の右辺
+    def __init__(self,first_term,second_term,recurrence_fomula):
+        self.first_term = first_term
+        self.second_term = second_term
+        self.recurrence_fomula = sympify(recurrence_fomula)
+
+    def enumeration(self,start,end):
+        _ = 1
+        while True:
+            if _ == 1:
+                num_log1 = self.first_term
+                num_log2 = self.second_term
+                _ += 1
+                if start == 1:
+                    yield num_log1
+            else:
+                num_log1,num_log2 = num_log2,self.recurrence_fomula.subs({"a_n":num_log1,"a_n_plus1":num_log2,"n":_})
+                _ += 1
+            if start <= _ <= end:
+                yield num_log2
+            if _ > end:
+                break
